989d23d0ae87422950f7c6c08105ac2f3ab8b227
Maksim Naumovich <ma31drom@gmail.com>
2017-04-13 22:31:39 +0300

    no message

--- src/main/java/com/epam/mentoring/App.java
+++ src/main/java/com/epam/mentoring/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
-	private App() {
+	public App() {
 	}
 
 	public static void main(final String[] args) {
