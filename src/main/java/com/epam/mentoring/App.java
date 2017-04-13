3b8c0f04d720ba37de332a8d2808ab0633dc3290
Maksim Naumovich <ma31drom@gmail.com>
2017-04-13 22:26:36 +0300

    sonr

--- src/main/java/com/epam/mentoring/App.java
+++ src/main/java/com/epam/mentoring/App.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 
+	private App() {
+	}
+
 	public static void main(final String[] args) {
 		System.setProperty("spring.profiles.active", "DEV");
 		SpringApplication.run(App.class, args);
